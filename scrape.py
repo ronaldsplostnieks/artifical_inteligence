eb0f6fdec09446542c08085cd05412252fcb16f0
Ronalds Plostnieks <ronalds.plostnieks@gmail.com>
2024-01-27 12:07:45 +0200

    majasdarbs

--- scrape.py
+++ scrape.py
@@ -4,21 +4,26 @@ from bs4 import BeautifulSoup as bs
 import csv
 import time
 
+
 URL = "https://www.ss.lv/lv/transport/cars/today-5/sell/"
 LAPAS = "lapas/"
 DATI = "dati/"
 
 def saglaba(url, datne):
     rezultats = requests.get(url)
+    print(rezultats.status_code)
     if rezultats.status_code == 200:
-        with open(datne, "w", encoding = 'utf-8') as fails:
+        with open(datne, 'w', encoding='utf-8') as fails:
             fails.write(rezultats.text)
     return
 
+# saglaba(URL, LAPAS+"pirma.html")
+
 def dabut_info(datne):
     dati = []
-    with open(datne, "r", encoding="UTF-8")as f:
+    with open(datne, "r", encoding="utf-8") as f:
         html = f.read()
+    
     zupa = bs(html, 'html.parser')
 
     galvena_dala = zupa.find(id='page_main')
@@ -26,13 +31,35 @@ def dabut_info(datne):
     tabulas = galvena_dala.find_all('table')
 
     rindas = tabulas[2].find_all('tr')
-
-    for rinda in rindas[1:]:
+    for rinda in rindas[1:-1]:
         lauki = rinda.find_all('td')
-        print(lauki)
-        auto = {}
-        auto['sludinajums_saite'] = lauki[1].find('a')['href']
-        print(auto['sludinajums_saite'])
 
-# saglaba(URL, LAPAS+"pirma.html")
-dabut_info("lapas/pirma.html")
+        auto = {}
+        auto['sludinajuma_saite'] = lauki[1].find('a')['href']
+        auto['bilde'] = lauki[1].find('img')['src']
+        auto['apraksts'] = lauki[2].get_text()
+        auto['marka'] = lauki[3].get_text()
+        auto['gads'] = lauki[4].get_text()
+        temp = lauki[5].get_text()
+        if temp[-1] == 'D':
+            auto['tips'] = 'Dīzelis'
+            auto['tilpums'] = temp[:-1]
+        elif  temp[-1] == 'E':
+            auto['tips'] = 'Elektro'
+            auto['tilpums'] = temp[:-1]
+        elif  temp[-1] == 'H':
+            auto['tips'] = 'Hibrīds'
+            auto['tilpums'] = temp[:-1]
+        else:
+            auto['tips'] = 'Benzīns'
+            auto['tilpums'] = temp  
+        auto['nobraukums'] = lauki[6].get_text()
+        auto['cena'] = lauki[7].get_text()
+        dati.append(auto)
+    return dati
+# Līdzīgā stilā iegūstiet informāciju par auto (aprakstu), marku, gadu, atsevišķi dzinēja tilpumu un tipu 
+# (benzīns/elektro/dīzelis/hibrīds), nobraukumu un cenu. Kur iespējams, cenšaties skaitļus no teksta pārveidot
+# par skaitļiem (izvilkt no teksta ārā).
+
+
+print(dabut_info(LAPAS+"pirma.html"))
