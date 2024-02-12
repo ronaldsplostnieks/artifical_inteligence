d5d67a17a259f0aacc21fa261efdea37878a3d72
Ronalds Plostnieks <ronalds.plostnieks@gmail.com>
2024-02-12 15:04:07 +0200

    m

--- bilzu_apstrade.py
+++ bilzu_apstrade.py
@@ -15,7 +15,9 @@ bilzu_adrese = 'bildes/'
 
 for nosaukums in os.listdir(bilzu_adrese):
     image = Image.open(os.path.join(bilzu_adrese,nosaukums))
-    # print(image)
-    # print(np.array(image))
-    # bildes.append(np.array(image))
+    bildes.append(np.array(image))
+    if "maja" in nosaukums:
+        label.append(1)
+    else:
+        label.append(0)
 
