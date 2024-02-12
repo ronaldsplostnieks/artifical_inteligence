67896f01d0e583b526449987310998318785deee
Ronalds Plostnieks <ronalds.plostnieks@gmail.com>
2024-02-12 14:58:37 +0200

    n

--- bilzu_apstrade.py
+++ bilzu_apstrade.py
@@ -5,3 +5,17 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score 
 from sklearn.ensemble import RandomForestClassifier
+
+
+bildes = []
+label = []
+
+bilzu_adrese = 'bildes/'
+
+
+for nosaukums in os.listdir(bilzu_adrese):
+    image = Image.open(os.path.join(bilzu_adrese,nosaukums))
+    # print(image)
+    # print(np.array(image))
+    # bildes.append(np.array(image))
+
