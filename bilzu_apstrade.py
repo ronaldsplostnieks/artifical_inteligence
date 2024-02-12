c000c654ec9df2d797934a9950a631ab6ef1024d
Ronalds Plostnieks <ronalds.plostnieks@gmail.com>
2024-02-12 14:46:03 +0200

    bildes

---
+++ bilzu_apstrade.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import numpy as np
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score 
+from sklearn.ensemble import RandomForestClassifier
